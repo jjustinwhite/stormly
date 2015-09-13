104c1a14d73ecadc5385e6849330fdd4a54bf33a
SirJustinWhite <Jjustinwhite@mac.com>
2015-09-12 22:02:24 -0500

    current location button updated with city text / disabled until JS loads to prevent error

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -14,16 +14,44 @@
 //= require jquery_ujs
 //= require_tree .
 
+function getCookie(name) {
+  var value = "; " + document.cookie;
+  var parts = value.split("; " + name + "=");
+  if (parts.length == 2) return parts.pop().split(";").shift();
+}
 
 function getGeoLocation() {
   navigator.geolocation.getCurrentPosition(setGeoCookie);
 }
 
 function setGeoCookie(position) {
+	//set lat_lng cookie
   var cookie_val = position.coords.latitude + "|" + position.coords.longitude;
-  document.cookie = "lat_lng=" + escape(cookie_val) + ';Path=/;';
+  document.cookie = "lat_lng=" + cookie_val + ';Path=/;';
+  	//split lat/lng 
+  var latlngStr = getCookie("lat_lng").split('|', 2);
+  var latlng = {lat: parseFloat(latlngStr[0]), lng: parseFloat(latlngStr[1])};
+  var jsonURL = "https://maps.googleapis.com/maps/api/geocode/json?latlng=" + latlng["lat"] + "," + latlng["lng"] + "&key=AIzaSyAqB4yFSNATtAmaE9xmlDZt9-adJx5Fz6c";
+  	//read JSON data
+  $.ajax({ 
+                type: 'GET', 
+                url: jsonURL, 
+                data: { get_param: 'value' }, 
+                dataType:'json',
+                success: function (data) { 
+                	//get city and state
+                       var city = data["results"][0]["address_components"][2]["long_name"];
+                       var state = data["results"][0]["address_components"][5]["short_name"];
+                    //set location arrow icon, city / state as button text, enable button
+                       document.getElementById("spinnerIcon").className = "fa fa-location-arrow";      
+                       document.getElementById("btnText").innerHTML = "&nbsp; " + city + ", " + state;
+  					   document.getElementById("locationBtn").disabled = false;
+                }
+            });
+
 }
 
 function deleteCookie() {
 	document.cookie = 'lat_lng=; Path=/; Expires=Thu, 01 Jan 1970 00:00:01 GMT;';
 }
+
